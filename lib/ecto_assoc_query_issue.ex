90a5d72a1b448529180c25d169411c581f9d9801
Eugene Pirogov <iamexile@gmail.com>
2018-11-23 17:41:37 +0200

    Initial commit

---
+++ lib/ecto_assoc_query_issue.ex
@@ -0,0 +1,18 @@
+defmodule EctoAssocQueryIssue do
+  @moduledoc """
+  Documentation for EctoAssocQueryIssue.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoAssocQueryIssue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_assoc_query_issue_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoAssocQueryIssueTest do
+  use ExUnit.Case
+  doctest EctoAssocQueryIssue
+
+  test "greets the world" do
+    assert EctoAssocQueryIssue.hello() == :world
+  end
+end
