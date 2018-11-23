1dcf7b4d96718cc9d7d64107b013609c4e1a7970
Eugene Pirogov <iamexile@gmail.com>
2018-11-23 17:45:03 +0200

    Remove comments

--- lib/ecto_assoc_query_issue.ex
+++ lib/ecto_assoc_query_issue.ex
@@ -1,17 +1,4 @@
 defmodule EctoAssocQueryIssue do
-  @moduledoc """
-  Documentation for EctoAssocQueryIssue.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EctoAssocQueryIssue.hello()
-      :world
-
-  """
   def hello do
     :world
   end
